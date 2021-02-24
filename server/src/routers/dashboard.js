7fdd1ff04a2c4b997afec792cbac5e1a3d83a1c8
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-02-25 00:09:38 +0530

    adds user api

--- server/src/routers/dashboard.js
+++ server/src/routers/dashboard.js
@@ -1,17 +1,9 @@
 import express from 'express';
+import {getUser,  updateUser } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({
-      error: null,
-      data: {
-        title: "My dashboard",
-        content: "dashboard content",
-        user: req.user, // token payload information
-      },
-    });
-  });
-
+  router.get('/user/:id',getUser);
+  router.put("/user/:id",updateUser);
   
   export default router;

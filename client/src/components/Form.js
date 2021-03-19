0f852ed0ad0c5547950a291a0b1bd236983053a9
13lav <lavsaini013@gmail.com>
2021-03-19 23:06:55 +0530

    Fix Api Connection

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -9,9 +9,21 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        display: 'flex'
+    },
+
+    list: {
         width: '50%',
         marginLeft: '2%',
         marginTop: '2%',
+    },
+    preview: {
+        width: '40%',
+        height: '80%',
+        marginRight: '2%',
+        marginLeft: '2%',
+        marginTop: '2%',
+        backgroundColor: '#fff'
     },
     heading: {
         fontSize: theme.typography.pxToRem(15),
@@ -39,128 +51,132 @@ export default function ControlledAccordions() {
 
     return (
         <div className={classes.root}>
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1bh-content"
-                    id="panel1bh-header"
-                >
-                    <Typography className={classes.heading}>Personal Info</Typography>
-                    <Typography className={classes.secondaryHeading}>Your Name, Email, Contact</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form noValidate autoComplete="off">
-                        <TextField className={classes.inputBox} id="standard-basic" label="Full Name" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Email" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Phone No." />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Location" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Profile Link" />
-                    </form>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2bh-content"
-                    id="panel2bh-header"
-                >
-                    <Typography className={classes.heading}>Education</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Your Educational Background
+            <div className={classes.list}>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Typography className={classes.heading}>Personal Info</Typography>
+                        <Typography className={classes.secondaryHeading}>Your Name, Email, Contact</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <form noValidate autoComplete="off">
+                            <TextField className={classes.inputBox} id="standard-basic" label="Full Name" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Email" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Phone No." />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Location" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Profile Link" />
+                        </form>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2bh-content"
+                        id="panel2bh-header"
+                    >
+                        <Typography className={classes.heading}>Education</Typography>
+                        <Typography className={classes.secondaryHeading}>
+                            Your Educational Background
           </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form noValidate autoComplete="off">
-                        <TextField className={classes.inputBox} id="standard-basic" label="School Name" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="School Location" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Degree" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Major" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Start Date" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="End Date" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="GPA" />
-                    </form>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel3bh-content"
-                    id="panel3bh-header"
-                >
-                    <Typography className={classes.heading}>Skills</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Your Skills/Expertise
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <form noValidate autoComplete="off">
+                            <TextField className={classes.inputBox} id="standard-basic" label="School Name" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="School Location" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Degree" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Major" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Start Date" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="End Date" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="GPA" />
+                        </form>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel3bh-content"
+                        id="panel3bh-header"
+                    >
+                        <Typography className={classes.heading}>Skills</Typography>
+                        <Typography className={classes.secondaryHeading}>
+                            Your Skills/Expertise
           </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form noValidate autoComplete="off">
-                        <TextField className={classes.inputBox} id="standard-basic" label="Skill Name" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Skill Details" />
-                    </form>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel4bh-content"
-                    id="panel4bh-header"
-                >
-                    <Typography className={classes.heading}>Projects</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Your Projects
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <form noValidate autoComplete="off">
+                            <TextField className={classes.inputBox} id="standard-basic" label="Skill Name" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Skill Details" />
+                        </form>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel4bh-content"
+                        id="panel4bh-header"
+                    >
+                        <Typography className={classes.heading}>Projects</Typography>
+                        <Typography className={classes.secondaryHeading}>
+                            Your Projects
                     </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form noValidate autoComplete="off">
-                        <TextField className={classes.inputBox} id="standard-basic" label="Project Name" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Project Description" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Project Link" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Tools Used" />
-                    </form>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel5bh-content"
-                    id="panel3bh-header"
-                >
-                    <Typography className={classes.heading}>Work-Ex</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Your Previous Work Experience
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <form noValidate autoComplete="off">
+                            <TextField className={classes.inputBox} id="standard-basic" label="Project Name" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Project Description" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Project Link" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Tools Used" />
+                        </form>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel5bh-content"
+                        id="panel3bh-header"
+                    >
+                        <Typography className={classes.heading}>Work-Ex</Typography>
+                        <Typography className={classes.secondaryHeading}>
+                            Your Previous Work Experience
           </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form noValidate autoComplete="off">
-                        <TextField className={classes.inputBox} id="standard-basic" label="Company Name" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Job Title" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Job Location" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Start Date" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Responsibilities" />
-                    </form>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel6bh-content"
-                    id="panel4bh-header"
-                >
-                    <Typography className={classes.heading}>Certifications</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Your Awards and Accompolishments
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <form noValidate autoComplete="off">
+                            <TextField className={classes.inputBox} id="standard-basic" label="Company Name" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Job Title" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Job Location" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Start Date" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Responsibilities" />
+                        </form>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel6bh-content"
+                        id="panel4bh-header"
+                    >
+                        <Typography className={classes.heading}>Certifications</Typography>
+                        <Typography className={classes.secondaryHeading}>
+                            Your Awards and Accompolishments
                     </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <form noValidate autoComplete="off">
-                        <TextField className={classes.inputBox} id="standard-basic" label="Award Name" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Award Date" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Award Date" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Awarder" />
-                        <TextField className={classes.inputBox} id="standard-basic" label="Summary" />
-                    </form>
-                </AccordionDetails>
-            </Accordion>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <form noValidate autoComplete="off">
+                            <TextField className={classes.inputBox} id="standard-basic" label="Award Name" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Award Date" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Award Date" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Awarder" />
+                            <TextField className={classes.inputBox} id="standard-basic" label="Summary" />
+                        </form>
+                    </AccordionDetails>
+                </Accordion>
+            </div>
+            <div className={classes.preview}>
+            </div>
         </div>
     );
 }

afbdb3964632f20475f6e260206bf2c5a6ff4dba
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-03-28 16:29:22 +0530

    adds password forgot/reset functionality

--- server/src/routers/auth.js
+++ server/src/routers/auth.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import {Sign,Login} from '../controllers/auth.js';
+import {Sign,Login,ForgotPassword,ResetPassword} from '../controllers/auth.js';
 
 
 const router = express.Router();
 
 router.post('/signup', Sign);
 router.post('/login', Login);
+
+router.post('/password/forgot',ForgotPassword);
+router.post('/password/reset',ResetPassword);
 
 export default router;

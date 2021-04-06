6b998dc1f7ec376e1a1a74513efa64fe4cc129e8
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-04-07 01:39:20 +0530

    adds templates

---
+++ client/src/components/templates/Temaplate4.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Template4 = (props) => {
+    return(
+        <div className="container">
+
+            <div></div>
+
+            
+
+
+        </div>
+    )
+}
+
+export default Template4;
---
+++ client/src/components/templates/Template2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Template2 = (props) => {
+    return(
+        <div className="container">
+
+            <div></div>
+
+            
+
+
+        </div>
+    )
+}
+
+export default Template2;

f5ef4cf97ea75e575551a3e00cb9d8d7300083c6
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-05-04 03:33:09 +0530

    adds forgot password component

---
+++ client/src/components/ForgotPassword.js
@@ -0,0 +1,143 @@
+import { React, useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useHistory } from "react-router-dom";
+import Alert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+          marginTop: theme.spacing(2),
+        },
+      },
+    paper: {
+        marginTop: theme.spacing(4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function ForgotPassword() {
+    const classes = useStyles();
+    const history = useHistory();
+
+    const [values, setValues] = useState({
+        email: '',
+        open: false,
+        error: ''
+    })
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value })
+    }
+
+    const goto = (res, user) => {
+        if (res.status === 200) {
+            history.push("/login", user)
+        }else{
+
+            //alert("Email is wrong");
+          return(
+            <div className={classes.root}>
+                <Alert variant="filled" severity="error">
+                    Email is wrong
+                </Alert>
+          </div>
+          )
+        }
+    }
+
+    const create = async (user) => {
+        try {
+            let response = await fetch('http://localhost:3000/api/password/forgot', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+            let res = await response.json()
+            console.log(res)
+            goto(response, res.user)
+            return response
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const clickSubmit = (event) => {
+        event.preventDefault();
+        const user = {
+            email: values.email || undefined,
+        }
+        create(user).then((data) => {
+            if (data.error) {
+                setValues({ ...values, error: data.error })
+            } else {
+                setValues({ ...values, error: '', open: true })
+            }
+        })
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <VpnKeyIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Forgot Password?
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                onChange={handleChange('email')}
+                                value={values.email}
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={clickSubmit}
+                    >
+                        Reset
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}

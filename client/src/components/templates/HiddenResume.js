4893f385128da95dda7da248f52000403e429b9b
13lav <lavsaini013@gmail.com>
2021-05-03 20:54:19 +0530

    Fix - Hidden template render

--- client/src/components/templates/HiddenResume.js
+++ client/src/components/templates/HiddenResume.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Template1 from './template1/Template1'
 
-const useStyles = makeStyles((theme) => ({
-    hidden: {
-        display: 'none'
-    },
-}));
-
 const HiddenResume = (props) => {
-    const classes = useStyles();
 
     return (
-        <Template1 className={classes.hidden} />
+        <Template1 />
     )
 
 }

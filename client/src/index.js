2841505eb6d3dc643feb4c5ae0079c164b04fc66
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-05-09 00:40:32 +0530

    adds react-alert

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// optional configuration
+const options = {
+    // you can also just use 'bottom center'
+    position: positions.BOTTOM_CENTER,
+    timeout: 2000,
+    offset: '30px',
+    // you can also just use 'scale'
+    transition: transitions.SCALE
+  }
+
+  
+  const Root = () => (
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  )
+
+ReactDOM.render(<Root />, document.getElementById('root'));

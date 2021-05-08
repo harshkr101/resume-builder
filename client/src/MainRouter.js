53839a50071463c71128cbca4a52a80d17292c52
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-05-09 01:55:56 +0530

    completes password reset

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -5,9 +5,8 @@ import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-import Template1 from './components/templates/template1/Template1';
-import Template2 from './components/templates/template2/Template2';
 import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
 
 const MainRouter = () => {
   return (
@@ -18,9 +17,8 @@ const MainRouter = () => {
         <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/password/forgot"  component={ForgotPassword} />
+        <Route path="/password/reset"  component={ResetPassword} />
         <Route path="/dashboard" component={Dashboard} />
-        <Route path="/template1" component={Template1} />
-        <Route path="/template2" component={Template2} />
       </Switch>
     </div>
   );
---
+++ client/src/components/ResetPassword.js
@@ -0,0 +1,191 @@
+import { React, useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useHistory } from "react-router-dom";
+import { useAlert } from "react-alert";
+import {useLocation} from "react-router-dom";
+import jwt from 'jsonwebtoken';
+
+const useStyles = makeStyles((theme) => ({
+    
+    paper: {
+        marginTop: theme.spacing(4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function ResetPassword() {
+    const classes = useStyles();
+    const history = useHistory();
+    const alert = useAlert();
+    const query = useQuery();
+
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+        open: false,
+        error: ''
+    })
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value })
+    }
+
+
+    function useQuery() {
+        return new URLSearchParams(useLocation().search);
+    }
+
+    const getToken = () =>{
+        let token = query.get("token");
+        return token;
+    }
+
+    const verifyToken = (token) =>{
+        jwt.verify(getToken(), 'resume', function(err, decoded) {
+          });
+    }
+
+
+
+    const goto = (res) => {
+        console.log("result:",res);
+       
+        if (res.status === 200) {
+           
+            alert.success("Password reset successfull");
+            alert.info("Redirecting you to login page")
+            setTimeout(() => {
+                history.push("/login");
+            }, 2000); 
+            
+        }else{
+           alert.error("Invalid token");
+        }
+    }
+
+    
+
+    const create = async (user) => {
+        try {
+            let response = await fetch('http://localhost:3000/api/password/reset', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+            goto(response);
+            return response
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const clickSubmit = (event) => {
+        event.preventDefault();
+
+        const token = getToken();
+        const payload = jwt.decode(token);
+        const userEmail = payload.email;
+
+        const user = {
+            password: values.password || undefined,
+            email: userEmail|| undefined,
+            token: token    || undefined
+        }
+
+        if(verifyToken()){
+            create(user).then((data) => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error })
+                } else {
+                    setValues({ ...values, error: '', open: true })
+                }
+            })
+        }else{
+            alert.error("Invalid token");
+            setTimeout(() => {
+                history.push("/login");
+            }, 2000); 
+        }
+
+        
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <VpnKeyIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Reset Password
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>        
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="password"
+                                onChange={handleChange('password')}
+                                value={values.password}
+                                label="Password"
+                                name="password"
+                                autoComplete="password"
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="confirmPassword"
+                                onChange={handleChange('confirmPassword')}
+                                value={values.confirmPassword}
+                                label="Confirm Password"
+                                name="confirmPassword"
+                                autoComplete="Confirm Password"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={clickSubmit}
+                    >
+                        Reset
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}

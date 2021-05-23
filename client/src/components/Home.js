0bdc38ed1deca39826ebc3cf70c464a132e30f08
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-05-24 00:32:47 +0530

    completes homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -39,79 +39,155 @@ const Home = (props) => {
 
     return (
         
-            <div className="container-fluid">
+            <div className="page-wrapper">
 
-                <div className="main">
+                <div className="main container-fluid">
 
-                <section className="top row" >
-                         <div className="col-5 left">
-                             <div className=" heading-content align-middle">
-                                <span className="main-heading">Build your resume today</span>
+                    <section className="top row" >
+                            <div className="col-5 left">
+                                <div className=" heading-content align-middle">
+                                    <span className="main-heading">Build your resume today</span>
+                                    <br></br>
+                                    <span className="main-subheading">Build a resume tailored to your needs</span>
+                                </div>
                                 <br></br>
-                                <span className="main-subheading">Build a resume tailored to your needs</span>
+                                <div>
+                                    <button className="btn btn-primary btn-lg" onClick={handleClick}>BUILD MY RESUME</button>
+                                </div>
                             </div>
-                            <br></br>
-                            <div>
-                                <button className="btn  btn-lg" onClick={handleClick}>BUILD RESUME</button>
+
+                            <div className="col-7 right ">
+                                <img  src={publicURL+"/assets/resume.png"}   alt=" resume"/>
                             </div>
-                         </div>
+                    </section>
 
-                         <div className="col-7 right ">
-                             <img  src={publicURL+"/assets/resume.png"}   alt=" resume"/>
-                         </div>
-                </section>
+                    <section className="start ">
+                        
+                        <div className="start-heading ">
+                            <span >How to Start?</span>
+                        </div>
 
-                <section className="start ">
                     
-                    <div className="start-heading ">
-                        <span >How to Start?</span>
-                    </div>
 
-                   
+                        <div className="content row align-content-center">
 
-                    <div className="content row align-content-center">
+                            <div className="card-wrapper first col-md">
 
-                        <div className="card-wrapper first col-md">
+                            <div className="card">
+                                <div className="card-heading">1</div>
+                                <div className="card-body">
+                                    <h5 className="card-title">Fill the form</h5>
+                                    <p className="card-text">Fill out your details in <strong>6</strong> easy steps</p>
 
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Fill the form</h5>
-                                <p className="card-text">Fill out your details in six easy steps</p>
+                                </div>
+                            </div>
+                            </div>
+
+                            <div className="card-wrapper col-md">
+
+                            <div className="card">
+                            <div className="card-heading">2</div>
+                                <div className="card-body">
+                                    <h5 className="card-title">Choose template</h5>
+                                    <p className="card-text">Choose a template from  provided templates</p>
+
+                                </div>
+                            </div>
 
                             </div>
-                        </div>
-                        </div>
 
-                        <div className="card-wrapper col-md">
+                            <div className="card-wrapper col-md">
+                            <div className="card col-md">
+                            <div className="card-heading">3</div>
+                                <div className="card-body">
+                                    <h5 className="card-title">Download Resume</h5>
+                                    <p className="card-text">That's it! Now you can download your resume in pdf form</p>
 
-                        <div className="card">
-                           
-                            <div className="card-body">
-                                <h5 className="card-title">Choose template</h5>
-                                <p className="card-text">Choose a template from multiple provided templates</p>
+                                </div>
+                            </div>
+                            </div>
+
+                        </div>
+                    </section>
+
+                    <section className="features ">
+                    
+                    <div className="features-heading "><h1>Features</h1></div>
+
+                    <div className="features-content align-items-center justify-content-center ">
+
+
+
+                        <div className="wrapper row">
+
+                                <div className="text-content col-md">
+                                    <div className="heading">Blazingly Fast</div>
+                                    <div className="text">
+                                        With out easy to use services you can build 
+                                        your resume in less than 10 minutes
+                                    </div>
+                                </div>
+
+                                <div className="image-content col-md">
+                                    <img  src={publicURL+'/assets/fast.svg'} alt="fast"/>
+                                </div>
 
                             </div>
-                        </div>
 
-                        </div>
 
-                        <div className="card-wrapper col-md">
-                        <div className="card col-md">
-                           
-                            <div className="card-body">
-                                <h5 className="card-title">Download Resume</h5>
-                                <p className="card-text">That's it! Now you can download your resume in pdf form</p>
+                            <div className="wrapper row">
+                            <div className="image-content col-md">
+                                <img  src={publicURL+'/assets/plan.svg'} alt="template"/>
 
                             </div>
-                        </div>
-                        </div>
 
-                    </div>
-                </section>
+                            <div className="text-content col-md">
+                                <div className="heading">Ready to use templates</div>
+                                    <div className="text">
+                                        We provide multiple ready to use templates to 
+                                        speed up resume building process
+                                    </div>
+                            </div>
+                            </div>
+
+                            
+
+                            <div className="wrapper row">
+                                <div className="text-content col-md">
+                                    <div className="heading">Free of Cost</div>
+                                    <div className="text">
+                                        We do not charge you any money for our services
+                                        <br></br>
+                                        But we encourage you to donate, it keeps our services running
+                                    </div>
+                                </div>
+
+                                <div className="image-content col-md">
+                                    <img  src={publicURL+'/assets/payment.svg'} alt="money"/>
+                                </div>
+                            </div>
+
+
+                            <div className="wrapper row">
+                            <   div className="image-content col-md">
+                                    <img  src={publicURL+'/assets/design.svg'} alt="design"/>
+                                </div>
+                                <div className="text-content col-md">
+                                    <div className="heading">Design your own resume</div>
+                                    <div className="text">
+                                        With our easy to use services you can create resume best suited to your needs
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </section> 
+
+                        <section className="build-wrapper ">
+                            <div className="build-resume align-items-center justify-content-center">
+                                <button className="btn btn-primary btn-lg">BUILD MY RESUME</button>
+                            </div>
+                        </section>
                 </div>
-
-              
-
             </div>
     )
 

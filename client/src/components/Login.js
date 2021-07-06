6b75f01639f43725d5bfa336dc4e1128af69617e
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-07-07 00:20:12 +0530

    fix

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -175,7 +175,7 @@ const Login = (props) => {
                                 </Link>
                             </Grid>
                             <Grid item>
-                                <Link href="/signup" variant="body2">
+                                <Link to="/signup" variant="body2">
                                     Don't have an account? Sign Up
                                 </Link>
                             </Grid>
